ac2aa3168658c36bd1d5a5311dc1f788faa92ea3
Mr. Samuel <82917953+lermal@users.noreply.github.com>
2025-11-28 12:58:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
 });
